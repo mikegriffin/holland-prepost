ceed9ce731d8373ba315de93f62ac6065ff9152c
Mike Griffin <mg@webmail.us>
2014-05-06 02:27:32 +0000

    minor cleanup of holland_post_xtrabackup_postcompress.sh

--- holland_post_xtrabackup_postcompress.sh
+++ holland_post_xtrabackup_postcompress.sh
@@ -3,7 +3,15 @@
 ###
 ## This script is intended to be run as after-backup-command for holland-xtrabackup
 ## When backupset's compression=none, the prepare (apply-log) phase happens by default
-## This script will then tar and compress the prepared backup in post
+## This script will then tar and compress the prepared backup in post when defined as follows:
+## 
+## [holland:backup]
+## plugin = xtrabackup
+## backups-to-keep = 0
+## auto-purge-failures = yes
+## purge-policy = before-backup
+## estimated-size-factor = 1.3
+## after-backup-command=/usr/local/bin/holland_post_xtrabackup_postcompress.sh ${backupdir}
 ###
 
 ## Set bash options
@@ -13,11 +21,12 @@ shopt -s extglob
 ## Ensure proper invokation of this script
 [[ "${#}" -eq 1 && -n "${1}" ]]  ||  { echo 'Bad arugments. Expecting Holland to invoke with ${backupdir} as the only argument.' >&2; exit 1; }
 
+## Set variables
 backup_dir="${1}"
 holland_destination="${backup_dir}/data"
 backup_root="${backup_dir}/backup_root"
 
-## Backup this script
+## Backup this script itself
 cp -a "${0}" "${backup_dir}" || exit 1
 
 ## Rename the "data" directory to "backup_root" and create an empty "data" directory
@@ -25,8 +34,8 @@ mv "${holland_destination}" "${backup_root}" || exit 1
 mkdir -p "${holland_destination}" || exit 1
 cd "${backup_root}" || exit 1
 
-## Default to using tar piped to single threaded gzip
 echo "Compress backup data"
+## Default to using tar piped to single threaded gzip
 /bin/tar --group=mysql --owner=mysql -cf - . | gzip --fast > "${holland_destination}"/backup.tar.gz
 ## Use tar piped to pigz for systems with more than 8 CPU cores
 #/bin/tar --group=mysql --owner=mysql -cf - . | pigz --fast -p4 > "${holland_destination}"/backup.tar.gz

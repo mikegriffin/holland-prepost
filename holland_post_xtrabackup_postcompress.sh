259f970dd993103a402f4cfa4caa2b3c5c90e88c
Mike Griffin <mg@webmail.us>
2014-05-06 22:26:02 +0000

    minor cleanup of holland_post_xtrabackup_postcompress.sh

--- holland_post_xtrabackup_postcompress.sh
+++ holland_post_xtrabackup_postcompress.sh
@@ -2,26 +2,27 @@
 
 ###
 ## This script is intended to be run as after-backup-command for holland-xtrabackup
-## When backupset's compression=none, the prepare (apply-log) phase happens by default
-## This script will then tar and compress the prepared backup in post when defined as follows:
+## When compression and streaming are disabled, the prepare (apply-log) phase is possible
+## This script will then tar the prepared backup in post
+## Required settings for the backupset
 ## 
 ## [holland:backup]
 ## plugin = xtrabackup
-## backups-to-keep = 0
-## auto-purge-failures = yes
-## purge-policy = before-backup
 ## estimated-size-factor = 1.3
 ## after-backup-command=/usr/local/bin/holland_post_xtrabackup_postcompress.sh ${backupdir}
+##
+## [xtrabackup]
+## stream = no
+## apply-logs = yes
 ###
 
-## Set bash options
+## Error on unset variables
 set -o nounset
-shopt -s extglob
 
 ## Ensure proper invokation of this script
 [[ "${#}" -eq 1 && -n "${1}" ]]  ||  { echo 'Bad arugments. Expecting Holland to invoke with ${backupdir} as the only argument.' >&2; exit 1; }
 
-## Set variables
+## Set paths
 backup_dir="${1}"
 holland_destination="${backup_dir}/data"
 backup_root="${backup_dir}/backup_root"
@@ -31,7 +32,7 @@ cp -a "${0}" "${backup_dir}" || exit 1
 
 ## Rename the "data" directory to "backup_root" and create an empty "data" directory
 mv "${holland_destination}" "${backup_root}" || exit 1
-mkdir -p "${holland_destination}" || exit 1
+mkdir "${holland_destination}" || exit 1
 cd "${backup_root}" || exit 1
 
 echo "Compress backup data"
@@ -46,4 +47,6 @@ echo "Compress backup data"
 echo "Purge uncompressed data"
 cd "${backup_dir}" || exit 1
 rm -rf "${backup_root}" || exit 1
+## By default the parent directory does not have world read permissions
+## This chown allows the mysql system user to read these files if the parent allows it
 chown -R mysql: "${backup_dir}" || exit 1

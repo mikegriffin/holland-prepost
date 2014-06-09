0c0eb0f8ffeae66bb8eb9f0385a300686cb8e0f6
Mike Griffin <mg@webmail.us>
2014-06-09 21:53:51 +0000

    better logging

--- holland_post_xtrabackup_postcompress.sh
+++ holland_post_xtrabackup_postcompress.sh
@@ -22,21 +22,23 @@
 compression_level=1
 compression_max_cpu_cores=4
 
+## Log the intent of this post script to stderr, so it comes first in dry run
+echo "INFO: Compressing already prepared xtrabackup in after-backup-command" 1>&2
 
 ## Ensure proper invocation of this script
-[[ "${#}" -eq 1 && -n "${1}" ]]  ||  { echo "Bad arugments. Expecting Holland to invoke with ${backupdir} as the only argument." >&2; exit 1; }
+[[ "${#}" -eq 1 && -n "${1}" ]]  ||  { echo "Bad arugments. Expecting Holland to invoke with \${backupdir} as the only argument." >&2; exit 1; }
 
 ## Set paths
 backupdir="${1}"
 datadir="${backupdir}/data"
 
-## Backup this script itself
-cp -a "${0}" "${backupdir}" || exit 1
-
 ## Verify that this appears to be an uncompressed backup that has already been prepared
 [[ -f "${datadir}"/xtrabackup_logfile &&
    -f "${datadir}"/ib_logfile0 &&
    -f "${datadir}"/ib_logfile1 ]] ||  { echo "Backup not prepared or holland in dry-run. Set stream=no and apply-logs=yes in backupset" >&2; exit 1; }
+
+## Backup this script itself
+cp -a "${0}" "${backupdir}" || exit 1
 
 ## Create compressed tar of datadir in backupdir
 compression_level_valid=`echo $(((${compression_level}*2)/2))`
@@ -55,9 +57,15 @@ else
  fi
 fi
 
+## Echo peak usage while holding both an uncompressed copy and a compressed copy
+echo "$(date "+%y%m%d %H:%M:%S") Peak disk usage was: $(du -sh ${backupdir} | cut -f1)"
+
 ## Purge out uncompressed data in datadir
 echo "$(date "+%y%m%d %H:%M:%S") Purge uncompressed data"
 rm -rf "${datadir}" || exit 1
+
+## Echo final on disk size
+echo "$(date "+%y%m%d %H:%M:%S") Final size: $(du -sh ${backupdir} | cut -f1)"
 
 ## By default the parent directory does not have world read permissions
 ## This chown allows the mysql system user to read these files if the parent allows it
